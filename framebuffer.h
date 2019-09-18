0057f84a729f76941fc0e9b38515583bfa412244
Anish <anishbhobe@hotmail.com>
2019-09-18 18:57:32 +0530

    New scene test and framebuffer separation.
    New cluttered scene + framebuffer in a separate file.

---
+++ framebuffer.h
@@ -0,0 +1,67 @@
+#ifndef _FRAMEBUFFER_H
+#define _FRAMEBUFFER_H
+
+#include <thread>
+#include <vector>
+
+class single_thread_policy {
+public:
+	void lock(){};
+	void unlock(){};
+};
+
+class multi_thread_policy {
+	std::mutex locke;
+
+public:
+	void lock() { locke.lock(); }
+	void unlock() { locke.unlock(); }
+};
+
+template <class thread_policy>
+class framebuffer : public thread_policy {
+private:
+	int width;
+	int height;
+	std::vector<vec3> data_vector;
+
+public:
+	framebuffer(int width, int height) :
+			width(width),
+			height(height),
+			data_vector(width * height) {}
+
+	size_t size() const { return data_vector.size(); }
+	auto begin() { return data_vector.begin(); }
+	auto end() { return data_vector.end(); }
+	auto data() { return data_vector.data(); }
+
+	vec3 &operator[](int i) { return data_vector[i]; }
+	vec3 &operator()(int i, int j) { return data_vector[i + width * j]; }
+	template <typename TP>
+	[[nodiscard]] std::vector<vec3> operator+(framebuffer<TP> &other) {
+		this->lock();
+		std::vector<vec3> res(other.size());
+		std::transform(data_vector.begin(), data_vector.end(), other.begin(),
+				res.begin(), std::plus<vec3>());
+		this->unlock();
+		return res;
+	}
+	template <typename TP>
+	std::vector<vec3> &operator+=(framebuffer<TP> &other) {
+		this->lock();
+		std::transform(data_vector.begin(), data_vector.end(), other.begin(),
+				data_vector.begin(), std::plus<vec3>());
+		this->unlock();
+		return data_vector;
+	}
+};
+
+template <typename TP>
+void operator/=(framebuffer<TP> &buffer, float scalar) {
+	for (auto &element : buffer) {
+		element /= scalar;
+	}
+}
+
+#endif /* _FRAMEBUFFER_H */
--- main.cpp
+++ main.cpp
@@ -1,26 +1,18 @@
+#include "camera.h"
+#include "framebuffer.h"
+#include "hitable.h"
+#include "material.h"
+#include "ray.h"
+#include "rng.h"
+#include "sphere.h"
+#include "stbi_write.h"
+#include "world.h"
+#include <ThreadPool.h>
 #include <deque>
 #include <fstream>
 #include <iostream>
 #include <string>
 #include <vector>
-
-#include <ThreadPool.h>
-
-#include <vector>
-
-#include <ThreadPool.h>
-
-#include "camera.h"
-#include "hitable.h"
-#include "material.h"
-#include "ray.h"
-#include "sphere.h"
-#include "world.h"
-
-#include "rng.h"
-
-#define STB_IMAGE_WRITE_IMPLEMENTATION
-#include "stbi_write.h"
 
 vec3 color(const ray &r, const hitable *world, int depth) {
 	hit_record rec;
@@ -42,86 +34,32 @@ vec3 color(const ray &r, const hitable *world, int depth) {
 	}
 }
 
-class single_thread_policy {
-public:
-	void lock(){};
-	void unlock(){};
-};
-
-class multi_thread_policy {
-	std::mutex locke;
-
-public:
-	void lock() { locke.lock(); }
-	void unlock() { locke.unlock(); }
-};
-
-template <class thread_policy>
-class framebuffer : public thread_policy {
-private:
-	int width;
-	int height;
-	std::vector<vec3> data_vector;
-
-public:
-	framebuffer(int width, int height) :
-			width(width),
-			height(height),
-			data_vector(width * height) {}
-
-	size_t size() const { return data_vector.size(); }
-	auto begin() { return data_vector.begin(); }
-	auto end() { return data_vector.end(); }
-	auto data() { return data_vector.data(); }
-
-	vec3 &operator[](int i) { return data_vector[i]; }
-	vec3 &operator()(int i, int j) { return data_vector[i + width * j]; }
-	template <typename TP>
-	[[nodiscard]] std::vector<vec3> operator+(framebuffer<TP> &other) {
-		this->lock();
-		std::vector<vec3> res(other.size());
-		std::transform(data_vector.begin(), data_vector.end(), other.begin(),
-				res.begin(), std::plus<vec3>());
-		this->unlock();
-		return res;
-	}
-	template <typename TP>
-	std::vector<vec3> &operator+=(framebuffer<TP> &other) {
-		this->lock();
-		std::transform(data_vector.begin(), data_vector.end(), other.begin(),
-				data_vector.begin(), std::plus<vec3>());
-		this->unlock();
-		return data_vector;
-	}
-};
-
-template <typename TP>
-void operator/=(framebuffer<TP> &buffer, float scalar) {
-	for (auto &element : buffer) {
-		element /= scalar;
-	}
-}
-
 int main(int argc, char *argv[]) {
-	unsigned int WIDTH = 200, HEIGHT = 100;
-	unsigned int NSAMPLES = 10;
+	struct {
+		unsigned int WIDTH = 200;
+		unsigned int HEIGHT = 100;
+		unsigned int NSAMPLES = 10;
+		unsigned int MAX_DEPTH = 25;
+	} config{};
 
 	std::ios_base::sync_with_stdio(false);
 	std::cout.tie(nullptr);
 
 	for (int i = 0; i < argc; i++) {
 		if ("-s" == std::string(argv[i])) {
-			NSAMPLES = atoi(argv[++i]);
+			config.NSAMPLES = atoi(argv[++i]);
 		} else if ("-r" == std::string(argv[i])) {
-			WIDTH = atoi(argv[++i]);
-			HEIGHT = atoi(argv[++i]);
+			config.WIDTH = atoi(argv[++i]);
+			config.HEIGHT = atoi(argv[++i]);
+		} else if ("-d" == std::string(argv[i])) {
+			config.MAX_DEPTH = atoi(argv[++i]);
 		}
 	}
 
-	vec3 eye = vec3(0, 0, -1);
+	vec3 eye = vec3(0, 0, 1);
 	vec3 target = vec3(0, 0, 0);
 	float focal_length = (target - eye).length();
-	camera cam(eye, target, vec3(0, 1, 0), 60.0f, (float)WIDTH / (float)HEIGHT,
+	camera cam(eye, target, vec3(0, 1, 0), 90.0f, (float)config.WIDTH / (float)config.HEIGHT,
 			0.0f, focal_length);
 
 	world w;
@@ -135,37 +73,42 @@ int main(int argc, char *argv[]) {
 	for (int i = -5; i < 5; i++) {
 		for (int j = -5; j < 5; j++) {
 			for (int k = -5; k < 5; k++) {
-				w.add<sphere>(vec3(i + 0.5f, j + 0.5f, k + 0.5f), 0.25f, new metallic(vec3(0.8f), 0.0f));
+				w.add<sphere>(vec3(-1, -1, -1) + vec3(rng(), rng(), rng()) - 0.5f, 10.f / 165.f, new lambertian(vec3(0.7f, 0.7f, 0.6f)));
 			}
 		}
 	}
+
+	w.add<sphere>(vec3(1, 0, 0), 0.5f, new metallic(vec3(0.8f), 0.0f));
+	w.add<sphere>(vec3(0, 0, 0), 0.5f, new dielectric(1.5f));
+	w.add<sphere>(vec3(0, 0, 0), -0.48f, new dielectric(1.5f));
 
 	w.compile();
 
 	auto optimal_threads = std::thread::hardware_concurrency();
 
-	framebuffer<multi_thread_policy> accumulator(WIDTH, HEIGHT);
+	framebuffer<multi_thread_policy> accumulator(config.WIDTH, config.HEIGHT);
 	{
 		ThreadPool pool(optimal_threads);
 
-		std::cerr << "RESOLUTION = " << WIDTH << "x" << HEIGHT << std::endl;
-		std::cerr << "SAMPLES = " << NSAMPLES << std::endl;
+		std::cerr << "RESOLUTION = " << config.WIDTH << "x" << config.HEIGHT << std::endl;
+		std::cerr << "SAMPLES = " << config.NSAMPLES << std::endl;
+		std::cerr << "MAX_DEPTH = " << config.MAX_DEPTH << std::endl;
 		std::cerr << "THREADS = " << optimal_threads << std::endl;
 
-		int count = NSAMPLES;
+		int count = config.NSAMPLES;
 
 		auto cbk = [&count]() {count--; std::cerr << '\r' << count; };
 		pool.register_callback(cbk);
 
-		for (int s = 0; s < NSAMPLES; s++) {
-			pool.enqueue([cam, HEIGHT, WIDTH, &accumulator, &w]() {
-				framebuffer<single_thread_policy> buffer(WIDTH, HEIGHT);
-				for (int j = 0; j < HEIGHT; j++) {
-					for (int i = 0; i < WIDTH; i++) {
-						float u = (i + rng()) / (float)WIDTH;
-						float v = (j + rng()) / (float)HEIGHT;
+		for (int s = 0; s < config.NSAMPLES; s++) {
+			pool.enqueue([cam, &config, &accumulator, &w]() {
+				framebuffer<single_thread_policy> buffer(config.WIDTH, config.HEIGHT);
+				for (int j = 0; j < config.HEIGHT; j++) {
+					for (int i = 0; i < config.WIDTH; i++) {
+						float u = (i + rng()) / (float)config.WIDTH;
+						float v = (j + rng()) / (float)config.HEIGHT;
 						ray r = cam.get_ray(u, v);
-						buffer[j * WIDTH + i] = color(r, &w, 25);
+						buffer[j * config.WIDTH + i] = color(r, &w, config.MAX_DEPTH);
 					}
 				}
 				accumulator += buffer;
@@ -173,8 +116,8 @@ int main(int argc, char *argv[]) {
 		}
 	}
 
-	accumulator /= NSAMPLES;
-	std::vector<uint8_t> img(WIDTH * HEIGHT * 3);
+	accumulator /= config.NSAMPLES;
+	std::vector<uint8_t> img(config.WIDTH * config.HEIGHT * 3);
 	int i = 0;
 	for (auto &pixel : accumulator) {
 		for (int j = 0; j < 3; j++) {
@@ -184,7 +127,9 @@ int main(int argc, char *argv[]) {
 	}
 
 	stbi_flip_vertically_on_write(true);
-	stbi_write_png("image.png", WIDTH, HEIGHT, 3, img.data(), 3 * WIDTH);
+	stbi_write_png("image.png", config.WIDTH, config.HEIGHT, 3, img.data(), 3 * config.WIDTH);
+
+	std::cerr << "\nRender Complete." << std::endl;
 
 	return 0;
 }
